b39aa890e556dacacd86a8866cf8cea9f74b721b
Ryan Baltenberger <ryan.baltenberger@gmail.com>
2015-01-05 17:44:31 -0500

    Taking axes off the net vis pdfs

--- src/visualize/visualize_layers.py
+++ src/visualize/visualize_layers.py
@@ -20,8 +20,9 @@ def vis_square(data, padsize=1, padval=0):
   # tile the filters into an image
   data = data.reshape((n, n) + data.shape[1:]).transpose((0, 2, 1, 3) + tuple(range(4, data.ndim + 1)))
   data = data.reshape((n * data.shape[1], n * data.shape[3]) + data.shape[4:])
-      
+  
   plt.imshow(data)
+  plt.axis('off')
 
 db_name = '../testing_data/testing_im/'
 db_labels_name = '../testing_data/testing_label/'
@@ -68,40 +69,41 @@ db = lmdb.open(db_name)
 with db.begin(write=False) as db_txn:
   for (key, value) in db_txn.cursor():
     dir = key.split('.')[0].replace('/','_')
-    print dir
-    try:
-      os.mkdir(dir)
-    except:
-      pass
+    if key == '00019919/20120905_165227.jpg':
+      print dir
+      try:
+        os.mkdir(dir)
+      except:
+        pass
 
-    im_datum = caffe.io.caffe_pb2.Datum().FromString(value)
-    im = caffe.io.datum_to_array(im_datum)
-    im = im.swapaxes(0,2).swapaxes(0,1)
+      im_datum = caffe.io.caffe_pb2.Datum().FromString(value)
+      im = caffe.io.datum_to_array(im_datum)
+      im = im.swapaxes(0,2).swapaxes(0,1)
 
-    #plt.imshow(im)
-    #plt.show()
-
-    # let caffe subtract mean and resize for me
-    caffe_input = net.preprocess('data', im)
-    caffe_input = caffe_input.reshape((1,)+caffe_input.shape)
-    
-		# push through the network
-    out = net.forward_all(data=caffe_input)
-    pred = out['fc8-t'].squeeze()
-
-    #pp([(k, v[0].data.shape) for k, v in net.params.items()])
-
-    filters = net.params['conv1'][0].data
-    vis_square(filters.transpose(0, 3, 2, 1))
-    #plt.show()
-    plt.savefig(path.join(dir, 'conv1' + '_weights' + '.pdf'))
-
-    #pp([(k, v.data.shape) for k, v in net.blobs.items()])
-
-    for k, v in net.blobs.items():
-      feat = net.blobs[k].data[0]
-      vis_square(feat, padval=1)
+      #plt.imshow(im)
       #plt.show()
-      plt.savefig(path.join(dir, k + '_blob' + '.pdf'))
 
-    sys.stdout.flush()
+      # let caffe subtract mean and resize for me
+      caffe_input = net.preprocess('data', im)
+      caffe_input = caffe_input.reshape((1,)+caffe_input.shape)
+      
+      # push through the network
+      out = net.forward_all(data=caffe_input)
+      pred = out['fc8-t'].squeeze()
+
+      #pp([(k, v[0].data.shape) for k, v in net.params.items()])
+
+      filters = net.params['conv1'][0].data
+      vis_square(filters.transpose(0, 3, 2, 1))
+      #plt.show()
+      plt.savefig(path.join(dir, 'conv1' + '_weights' + '.pdf'))
+
+      #pp([(k, v.data.shape) for k, v in net.blobs.items()])
+
+      for k, v in net.blobs.items():
+        feat = net.blobs[k].data[0]
+        vis_square(feat, padval=1)
+        #plt.show()
+        plt.savefig(path.join(dir, k + '_blob' + '.pdf'))
+
+      sys.stdout.flush()
