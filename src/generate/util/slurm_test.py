9417d51447be7e59a234c2e86737b130014df0a2
Ryan Baltenberger <ryan.baltenberger@gmail.com>
2015-03-31 14:58:58 -0400

    adding script to search for lowest testing loss in slurm/output file

---
+++ src/generate/util/slurm_test.py
@@ -0,0 +1,26 @@
+import glob
+import numpy as np
+
+slurms = glob.glob('jobs/**/*.out')
+
+min_loss = []
+min_loss_iter = []
+for slurm in slurms:
+  loss = []
+  iteration = []
+  with open(slurm, 'r') as f:
+    for line in f:
+      if 'Testing net' in line:
+        iteration.append(line.split()[5][:-1])
+      if 'Test net output' in line:
+        loss.append(line.split()[10])
+
+    min_loss.append(min(loss))
+    min_loss_iter.append(iteration[loss.index(min(loss))])
+
+min_loss = np.vstack(min_loss)
+min_loss_iter = np.vstack(min_loss_iter)
+min_loss_stacked = np.hstack([min_loss_iter, min_loss])
+
+for iter,loss in min_loss_stacked:
+  print iter, loss
