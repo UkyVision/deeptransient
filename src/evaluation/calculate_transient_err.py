85f7100828ded4c8e4269bcf8a1ff61868d41cd4
Ryan Baltenberger <ryan.baltenberger@gmail.com>
2015-04-01 17:00:05 -0400

    comments and auto net evaluation

--- src/evaluation/calculate_transient_err.py
+++ src/evaluation/calculate_transient_err.py
@@ -1,12 +1,27 @@
 import numpy as np
 
-data_c = np.loadtxt(open("data/fixed_tests/caffenet.txt","rb"), delimiter=",")
-data_cf = np.loadtxt(open("data/caffenet_frozen_102000.txt","rb"), delimiter=",")
-data_cf2 = np.loadtxt(open("data/caffenet_frozen_phase2_55000.txt","rb"), delimiter=",")
-data_cs = np.loadtxt(open("data/caffenet_slowburn_142000.txt","rb"), delimiter=",")
+#data_c = np.loadtxt(open("data/fixed_tests/caffenet.txt","rb"), delimiter=",")
+#data_cf = np.loadtxt(open("data/caffenet_frozen_102000.txt","rb"), delimiter=",")
+#data_cf2 = np.loadtxt(open("data/caffenet_frozen_phase2_55000.txt","rb"), delimiter=",")
+#data_cs = np.loadtxt(open("data/caffenet_slowburn_142000.txt","rb"), delimiter=",")
+#data_c99g = np.loadtxt(open("data/caffenet_99g_64000.txt","rb"), delimiter=",")
 #data_p = np.loadtxt(open("data/places_avg.txt","rb"), delimiter=",")
 #data_h = np.loadtxt(open("data/hybrid_avg.txt","rb"), delimiter=",")
 data_l = np.loadtxt(open("data/fixed_tests/new_paper.txt","rb"), delimiter=",")
 
-#print np.average(data_c), np.average(data_p), np.average(data_h), np.average(data_l)
-print np.average(data_c), np.average(data_cf2), np.average(data_l)
+
+data_100ss = np.loadtxt(open("data/caffenet_100ss_67000.txt","rb"), delimiter=",")
+data_200ss = np.loadtxt(open("data/caffenet_200ss_72000.txt","rb"), delimiter=",")
+data_300ss = np.loadtxt(open("data/caffenet_300ss_72000.txt","rb"), delimiter=",")
+data_400ss = np.loadtxt(open("data/caffenet_400ss_72000.txt","rb"), delimiter=",")
+data_500ss = np.loadtxt(open("data/caffenet_500ss_72000.txt","rb"), delimiter=",")
+data_600ss = np.loadtxt(open("data/caffenet_600ss_68000.txt","rb"), delimiter=",")
+data_1100ss = np.loadtxt(open("data/caffenet_1100ss_64000.txt","rb"), delimiter=",")
+
+print np.average(data_100ss), np.average(data_l)
+print np.average(data_200ss), np.average(data_l)
+print np.average(data_300ss), np.average(data_l)
+print np.average(data_400ss), np.average(data_l)
+print np.average(data_500ss), np.average(data_l)
+print np.average(data_600ss), np.average(data_l)
+print np.average(data_1100ss), np.average(data_l)
--- src/evaluation/compute_avgs.py
+++ src/evaluation/compute_avgs.py
@@ -5,7 +5,38 @@ import numpy as np
 import lmdb
 import h5py
 from matplotlib import pyplot as plt
+import glob
 
+stepsize_var = 0
+outfile = 'data/caffenet_lr_results.txt'
+
+#
+# get list of output files
+#
+slurms = glob.glob('../generate/jobs/**/*.out')
+
+#
+# search for minimum loss and its 
+# iteration in each output file
+#
+min_loss = []
+min_loss_iter = []
+for slurm in slurms:
+  loss = [] 
+  iteration = []
+  with open(slurm, 'r') as f:
+    for line in f:
+      if 'Testing net' in line:
+        iteration.append(line.split()[5][:-1])
+      if 'Test net output' in line:
+        loss.append(line.split()[10])
+    
+    min_loss.append(min(loss))
+    min_loss_iter.append(iteration[loss.index(min(loss))])
+
+#
+# load testing dbs
+#
 db_name = '../testing_data/test_shuffled_im_db/'
 db_labels_name = '../testing_data/test_shuffled_label_db/'
 
@@ -24,60 +55,64 @@ with db_labels.begin(write=False) as db_labels_txn:
 
 labels = np.vstack(labels)
 
-
 #
-# load the trained net 
+# evaluate each network
 #
+for iteration in min_loss_iter:
+  stepsize_var += 1000
 
-MODEL = '../prototxts/caffenet_frozen_phase2/deploy.prototxt'
-PRETRAINED = '../prototxts/caffenet_frozen_phase2/snapshots/caffenet_slow_fp2_iter_55000.caffemodel'
-MEAN = '../mean/transient_mean.binaryproto'
+  #
+  # load the trained net 
+  #
 
-# load the mean image 
-blob=caffe.io.caffe_pb2.BlobProto()
-file=open(MEAN,'rb')
-blob.ParseFromString(file.read())
-means = caffe.io.blobproto_to_array(blob)
-means = means[0]
+  #MODEL = '../prototxts/caffenet_frozen_phase2/deploy.prototxt'
+  #PRETRAINED = '../prototxts/caffenet_frozen_phase2/snapshots/caffenet_slow_fp2_iter_55000.caffemodel'
+  #MEAN = '../mean/transient_mean.binaryproto'
+  MODEL = '../generate/jobs/caffenet_%sss/deploy.prototxt' % stepsize_var
+  PRETRAINED = '../generate/jobs/caffenet_%sss/snapshots/caffenet_%sss_iter_%s.caffemodel' % (stepsize_var, stepsize_var, iteration)
+  MEAN = '../mean/transient_mean.binaryproto'
 
-net = caffe.Classifier(MODEL, PRETRAINED,
-    mean=means)
-net.set_phase_test()
-net.set_mode_cpu()
+  # load the mean image 
+  blob=caffe.io.caffe_pb2.BlobProto()
+  file=open(MEAN,'rb')
+  blob.ParseFromString(file.read())
+  means = caffe.io.blobproto_to_array(blob)
+  means = means[0]
 
-#
-# process 
-#
-ix = 0
-error = np.zeros(40)
-db = lmdb.open(db_name)
+  net = caffe.Classifier(MODEL, PRETRAINED,
+      mean=means)
+  net.set_phase_test()
+  net.set_mode_cpu()
 
-# get all keys
-with db.begin(write=False) as db_txn:
-  for (key, value) in db_txn.cursor():
-    im_datum = caffe.io.caffe_pb2.Datum().FromString(value)
-    im = caffe.io.datum_to_array(im_datum)
-    im = im.swapaxes(0,2).swapaxes(0,1)
+  #
+  # process 
+  #
+  ix = 0
+  error = np.zeros(40)
+  db = lmdb.open(db_name)
 
-    # let caffe subtract mean and resize for me
-    caffe_input = net.preprocess('data', im)
-    caffe_input = caffe_input.reshape((1,)+caffe_input.shape)
+  # get all keys
+  with db.begin(write=False) as db_txn:
+    for (key, value) in db_txn.cursor():
+      im_datum = caffe.io.caffe_pb2.Datum().FromString(value)
+      im = caffe.io.datum_to_array(im_datum)
+      im = im.swapaxes(0,2).swapaxes(0,1)
+
+      # let caffe subtract mean and resize for me
+      caffe_input = net.preprocess('data', im)
+      caffe_input = caffe_input.reshape((1,)+caffe_input.shape)
+      
+      # push through the network
+      out = net.forward_all(data=caffe_input)
+      pred = out['fc8-t'].squeeze()
+
+      # squared difference
+      error += ((pred[:] - labels[ix,:]) ** 2).squeeze()
     
-		# push through the network
-    out = net.forward_all(data=caffe_input)
-    #pred = out['fc8-t'].squeeze()
-    pred = out['fc8-t'].squeeze()
+      ix = ix + 1
 
-    error += ((pred[:] - labels[ix,:]) ** 2).squeeze()
-  
-    #print pred
-    sys.stdout.flush()
-
-    ix = ix + 1
-
-error = error[:] / ix
-for att in error:
-  sys.stdout.write(str(att))
-  sys.stdout.write(',')
-sys.stdout.write('\n')
-#print error
+  # write out to file
+  error = error[:] / ix
+  with open(outfile, 'a+') as f:
+    f.write(str(stepsize_var) + ' ' + str(iteration) + ' ' + str(np.average(error)) + '\n')
+  print np.average(error)
