63c3773331d74a40d09097f5dee5342317c622de
Ryan Baltenberger <ryan.baltenberger@gmail.com>
2015-05-19 12:51:31 -0400

    adding im names and some initial cca work. not correct yet. working on matching up the images for each camera

--- src/visualize/localization.m
+++ src/visualize/localization.m
@@ -34,16 +34,38 @@ hold on
 scatter(cam_locs(:,2), cam_locs(:,1), 'red', '.')
 
 %% load transient features
-imageset = '00007371';
+% load camera 1 features and image names
+imageset = '00005020';
 weightset = 'transientneth';
 layer = 'fc8-t';
 base_dir = sprintf('/u/eag-d1/scratch/ryan/webcams/%s/features/transientneth/', imageset);
 
-
-fNames = textscan(fopen(sprintf('/u/eag-d1/scratch/ryan/webcams/%s/features/transientneth/image_names.txt', imageset)), '%s');
 f_weights = [base_dir weightset '_features_' layer '.h5'];
-features = h5read(f_weights, '/features');
+features1 = h5read(f_weights, '/features');
+fNames1 = textscan(fopen(sprintf('/u/eag-d1/scratch/ryan/webcams/%s/features/transientneth/image_names.txt', imageset)), '%s');
+fNames1 = fNames1{1};
 
 
+% load camera 2 features and image names
+imageset = '00005021';
+weightset = 'transientneth';
+layer = 'fc8-t';
+base_dir = sprintf('/u/eag-d1/scratch/ryan/webcams/%s/features/transientneth/', imageset);
 
+f_weights = [base_dir weightset '_features_' layer '.h5'];
+features2 = h5read(f_weights, '/features');
+fNames2 = textscan(fopen(sprintf('/u/eag-d1/scratch/ryan/webcams/%s/features/transientneth/image_names.txt', imageset)), '%s');
+fNames2 = fNames2{1};
 
+%% cca and other experiments
+
+[A,B,r,U,V] = canoncorr(features1(:,1:9000)', features2(:,1:9000)');
+
+plot(U(:,1),V(:,1),'.')
+
+figure(1)
+imagesc(features1(:,1:9000))
+figure(2)
+imagesc(features2(:,1:9000))
+% figure(3)
+% imagesc(features1(:,1:9000) - features2(:,1:9000))
