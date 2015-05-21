126d43f75abe88aa0d18e86cb2bb732b6f353915
Ryan Baltenberger <ryan.baltenberger@gmail.com>
2015-05-21 16:46:41 -0400

    dates as xticks now work

--- src/visualize/attribute_vis.m
+++ src/visualize/attribute_vis.m
@@ -1,23 +1,35 @@
 attrs = csvread('data/9112_attributes.csv',0,1);
 attr_names = textscan(fopen('/u/eag-d1/scratch/ryan/transient/annotations/attributes.txt'), '%s\n');
 attr_names = attr_names{1};
+im_names = textscan(fopen('data/9112_attributes.csv'), strcat('%s', repmat('%*f',1,40), '\n'), 'Delimiter', ',');
+im_names = im_names{1};
+
+startDate = datenum('11-01-2013');
+endDate = datenum('11-30-2013');
+xData = linspace(startDate,endDate,size(attrs,1));
 
 figure(1);
 subplot(3,1,1);
-plot(attrs(:,2));
+plot(xData, attrs(:,2));
 ylim([0,1]);
 xlim([0,size(attrs,1)]);
 ylabel(attr_names(2));
+set(gca, 'XTick', xData(1:100:end));
+datetick('x', 1, 'keepticks');
 hold on;
 
 subplot(3,1,2);
-plot(attrs(:,3));
+plot(xData, attrs(:,3));
 ylim([0,1]);
 xlim([0,size(attrs,1)]);
 ylabel(attr_names(3));
+set(gca, 'XTick', xData(1:100:end));
+datetick('x', 1, 'keepticks');
 
 subplot(3,1,3);
-plot(attrs(:,10));
+plot(xData, attrs(:,10));
 ylim([0,1]);
 xlim([0,size(attrs,1)]);
 ylabel(attr_names(10));
+set(gca, 'XTick', xData(1:100:end));
+datetick('x', 1, 'keepticks');
