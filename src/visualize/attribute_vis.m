1d20f9aff01d2f882a2fd4b7000bb2f3053e5009
Ryan Baltenberger <ryan.baltenberger@gmail.com>
2015-05-22 12:36:02 -0400

    some changes to the attribute vis plot

--- src/visualize/attribute_vis.m
+++ src/visualize/attribute_vis.m
@@ -1,35 +1,46 @@
-attrs = csvread('data/9112_attributes.csv',0,1);
+addpath ~/matlab_root
+
+attrs_file = 'data/4795_attributes.csv';
+
+attrs = csvread(attrs_file,0,1);
 attr_names = textscan(fopen('/u/eag-d1/scratch/ryan/transient/annotations/attributes.txt'), '%s\n');
 attr_names = attr_names{1};
-im_names = textscan(fopen('data/9112_attributes.csv'), strcat('%s', repmat('%*f',1,40), '\n'), 'Delimiter', ',');
+im_names = textscan(fopen(attrs_file), strcat('%s', repmat('%*f',1,40), '\n'), 'Delimiter', ',');
 im_names = im_names{1};
 
-startDate = datenum('11-01-2013');
-endDate = datenum('11-30-2013');
-xData = linspace(startDate,endDate,size(attrs,1));
+for ix=1:size(attrs,1)
+    [~,im_date] = fileparts(im_names{ix});
+    im_dates{ix} = im_date;
+end
+xData = datenum(im_dates, 'yyyymmdd_HHMMSS');
+
+inds = find(xData >= datenum('02-16-2013') & xData <= datenum('02-24-2013'));
 
 figure(1);
 subplot(3,1,1);
-plot(xData, attrs(:,2));
+plot(xData(inds), attrs(inds,2));
 ylim([0,1]);
-xlim([xData(1),xData(end)]);
+xlim([xData(inds(1)),xData(inds(end))]);
 ylabel(attr_names(2));
-set(gca, 'XTick', xData(85:85:end));
-datetick('x', 1, 'keepticks', 'keeplimits');
+set(get(gca,'ylabel'), 'fontsize', 15);
+set(gca, 'XTick', xData(inds(30:40:end)));
+datetick('x', 'mmm-dd-yyyy HH:MM:SS', 'keepticks', 'keeplimits');
 hold on;
 
 subplot(3,1,2);
-plot(xData, attrs(:,3));
+plot(xData(inds), attrs(inds,3));
 ylim([0,1]);
-xlim([xData(1),xData(end)]);
+xlim([xData(inds(1)),xData(inds(end))]);
 ylabel(attr_names(3));
-set(gca, 'XTick', xData(85:85:end));
-datetick('x', 1, 'keepticks', 'keeplimits');
+set(get(gca,'ylabel'), 'fontsize', 15);
+set(gca, 'XTick', xData(inds(30:40:end)));
+datetick('x', 'mmm-dd-yyyy HH:MM:SS', 'keepticks', 'keeplimits');
 
 subplot(3,1,3);
-plot(xData, attrs(:,10));
+plot(xData(inds), attrs(inds,10));
 ylim([0,1]);
-xlim([xData(1),xData(end)]);
+xlim([xData(inds(1)),xData(inds(end))]);
 ylabel(attr_names(10));
-set(gca, 'XTick', xData(85:85:end));
-datetick('x', 1, 'keepticks', 'keeplimits');
+set(get(gca,'ylabel'), 'fontsize', 15);
+set(gca, 'XTick', xData(inds(30:40:end)));
+datetick('x', 'mmm-dd-yyyy HH:MM:SS', 'keepticks', 'keeplimits');
