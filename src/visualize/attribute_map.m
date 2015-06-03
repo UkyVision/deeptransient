5c35b7697baa775e1e8e993755feaa4d866c3456
Ryan Baltenberger <ryan.baltenberger@gmail.com>
2015-06-03 17:31:27 -0400

    adding code to visualize flickr images. map don't look good

--- src/visualize/attribute_map.m
+++ src/visualize/attribute_map.m
@@ -1,5 +1,5 @@
 addpath ~/matlab_root/
-set(0,'Format','long')
+set(0,'Format','longg')
 
 %% street view images
 %% load data
@@ -80,3 +80,82 @@ colormap(jet(256))
 title(attr{1}{attr_to_plot - 1})
 
 %% flickr images
+%% load data
+data_flickr = textscan(fopen('/u/amo-d0/grad/rbalten/research/scenic_labeling/data/flickr_transient_pred.csv'), strcat('%s%f%f%s',repmat('%f', 1, 40)), 'delimiter', ',');
+
+to_clear = isnan(data_flickr{end});
+
+for ind=1:size(data_flickr, 2)
+   data_flickr{ind}(to_clear) = []; 
+end
+
+% extract file names and make one big list
+fNames = data_flickr{4};
+
+% set up matrices to hold lat and lon coords
+data_lat = zeros(size(fNames, 1), 1);
+data_lon = zeros(size(fNames, 1), 1);
+
+% loop over each image and extract the lat lon coords
+% from the file name
+for ix=1:size(fNames, 1)
+    [~, coords, ~] = fileparts(fNames{ix});
+    splits = strsplit(coords, '_');
+    lat = str2double(splits(3));
+    lon = str2double(splits(4));
+        
+    data_lat(ix) = lat;
+    data_lon(ix) = lon;
+    
+    if mod(ix, 1000) == 0
+       fprintf('Processed %d of %d\n', ix, size(fNames,1)) 
+    end
+end
+
+% load the attribute names
+attr = textscan(fopen('/u/eag-d1/scratch/ryan/transient/annotations/attributes.txt'), '%s\n');
+
+%% loop through all attribute maps
+for j=5:44
+    % extract the attribute values and make one big list
+    attr_to_plot = j;
+    attr_val = data_flickr{attr_to_plot};
+
+    % remove alaska images
+    data_full = cat(2, data_lat, data_lon, attr_val);
+    data_full(data_full(:,2) < -130,:) = [];
+    
+    % make smooth map
+    [smooth_map, lon_centers, lat_centers, alpha] = make_smooth_map(data_full(:,3), [data_full(:,1) data_full(:,2)]);
+
+    % plot the smooth map
+    figure(1); clf;
+    axes('Position', [0 0 1 1]);
+    imagesc(smooth_map, 'XData', lon_centers, 'YData', lat_centers, 'AlphaData', alpha);
+    axis image xy off
+    %colorbar('SouthOutside')
+    colormap(jet(256))
+    title(attr{1}{j - 4})
+    pause
+end
+
+%% plot a single attribute map
+% extract attribute values and make one big list
+attr_to_plot = 40 + 4;
+attr_val = data_flickr{attr_to_plot};
+
+% remove alaska images
+data_full = cat(2, data_lat, data_lon, attr_val);
+data_full(data_full(:,2) < -130,:) = [];
+
+% make smooth map
+[smooth_map, lon_centers, lat_centers, alpha] = make_smooth_map(data_full(:,3), [data_full(:,1) data_full(:,2)]);
+
+% plot smooth map
+figure(1); clf;
+axes('Position', [0 0 1 1]);
+imagesc(smooth_map, 'XData', lon_centers, 'YData', lat_centers, 'AlphaData', alpha);
+axis image xy off
+%colorbar('SouthOutside')
+colormap(jet(256))
+title(attr{1}{attr_to_plot - 4})
