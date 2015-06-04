78f59597597843f22226be3c9ea335529b334027
Ryan Baltenberger <ryan.baltenberger@gmail.com>
2015-06-04 15:55:53 -0400

    working on script to compute better greenness index through filtering

---
+++ src/visualize/greenness.m
@@ -0,0 +1,15 @@
+addpath ~/matlab_root/
+
+% greenness_index = green / (red + green + blue)
+
+files = rdir('/u/eag-d1/scratch/ryan/amos_labeling/AMOS_Data/00007371/*/*.jpg');
+
+greenness_index = zeros(size(files,1), 1);
+for ix=1:size(files,1)
+    im = imread(files(ix).name);
+   
+    greenness = mean2(im(:,:,2)) / (mean2(im(:,:,1)) + mean2(im(:,:,2)) + mean2(im(:,:,3)));
+    
+    greenness_index(ix) = greenness;
+
+end
