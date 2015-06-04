26176d770a89229c007f4a5a457974d1d84cea40
Ryan Baltenberger <ryan.baltenberger@gmail.com>
2015-06-04 15:56:17 -0400

    forgot to save changes in matlab

--- src/visualize/greenness.m
+++ src/visualize/greenness.m
@@ -8,8 +8,11 @@ greenness_index = zeros(size(files,1), 1);
 for ix=1:size(files,1)
     im = imread(files(ix).name);
    
-    greenness = mean2(im(:,:,2)) / (mean2(im(:,:,1)) + mean2(im(:,:,2)) + mean2(im(:,:,3)));
+    greenness = mean2(im(end-200:end,1:200,2)) / (mean2(im(end-200:end,1:200,1)) + mean2(im(end-200:end,1:200,2)) + mean2(im(end-200:end,1:200,3)));
     
     greenness_index(ix) = greenness;
-
+    
+    if mod(ix, 100) == 0
+       fprintf('Processed %d of %d\n', ix, size(files,1))
+    end
 end
