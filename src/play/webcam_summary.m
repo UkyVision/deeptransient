652e70b6fecbc18f8d7e81ed7b1b45208020e818
Ryan Baltenberger <ryan.baltenberger@gmail.com>
2015-12-29 11:35:19 -0500

    fixing the no data problem in the colormap for webcam summaries

--- src/play/webcam_summary.m
+++ src/play/webcam_summary.m
@@ -87,7 +87,7 @@ attrs = textscan(fopen('fcn_fun/attributes.txt'), '%s'); attrs = attrs{1};
 figure(2); clf
 imagesc(cat(1,allofthem{[attr_id]}), [0 1]); axis image
 set(gca, 'YTick', []);
-colormap(interp1(linspace(0,1,3), [0.65 0 0; 0.7 0.7 0.7; 0 0.8 0], linspace(0,1,100)))
+colormap([1 1 1; interp1(linspace(0,1,3), [0.65 0 0; 0.7 0.7 0.7; 0 0.8 0], linspace(0,1,100))])
 axis image xy
 set(gca, 'XTick', []);
 set(gca, 'TickLength', [0 0]);
